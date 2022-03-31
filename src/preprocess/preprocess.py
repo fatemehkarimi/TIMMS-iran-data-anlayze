4b177262348e4b7deca656a7f3475fcb68c1340c
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-03-31 21:48:16 +0430

    visualize correlation between nominal attributes

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -4,7 +4,8 @@ import dataset.datasetConst as dataConst
 from preprocess_scale import ScalePreprocess
 from preprocess_ordinal import OrdinalPreprocess
 from preprocess_nominal import NominalPreprocess
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def preprocess_by_level(df, attr_list, level, preprocessor):
     for attr in attr_list:
@@ -12,6 +13,21 @@ def preprocess_by_level(df, attr_list, level, preprocessor):
             if attr.level == level:
                 df = preprocessor.fill_missing_value(df, attr)
     return df
+
+def detect_redundency_nominal(df, attr_list):
+    nominal_columns = [
+        attr.variable for attr in attr_list if attr.variable in df.columns]
+
+    nominal_df = df[nominal_columns]
+    correlations = nominal_df.corr()
+    fig = plt.figure(figsize=(150, 150))
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(correlations, vmin=-1, vmax=1)
+    fig.colorbar(cax)
+    ticks = np.arange(0, len(nominal_df.columns), 1)
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+    plt.savefig('nominal_correlation.png')
 
 
 def main():
@@ -42,6 +58,14 @@ def main():
         dataConst.AttrbuteLevel.NOMINAL,
         NominalPreprocess()
     )
+
+    nominal_attr_list = []
+    for attr in attr_list:
+        if (attr.level == dataConst.AttrbuteLevel.NOMINAL
+            and attr.variable not in dataConst.ID_FIELDS):
+            nominal_attr_list.append(attr)
+    
+    detect_redundency_nominal(filtered_df, nominal_attr_list)
 
     validated_df = pd.concat([df[dataConst.ID_FIELDS], filtered_df], axis=1)
     validated_df.to_excel("valid.xlsx")
