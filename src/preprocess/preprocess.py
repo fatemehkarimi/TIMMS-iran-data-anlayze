e5fd8c83b03be0445b22c03e479886553c2ac204
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-04-02 15:40:24 +0430

    remove correlated nominal attributes

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -6,6 +6,10 @@ from preprocess_scale import ScalePreprocess
 from preprocess_ordinal import OrdinalPreprocess
 from preprocess_nominal import NominalPreprocess
 
+import matplotlib.pyplot as plt
+from scipy.stats import f_oneway
+
+
 def fill_null_values(df, attr_list, level, preprocessor):
     level_attrs = [attr for attr in attr_list 
         if (attr.level == level
@@ -22,12 +26,69 @@ def visualize_correlation(df, attr_list, level, preprocessor):
     
     preprocessor.visualize_correlation(df, level_attrs)
 
+
+def visualize_nominal_scale_correlatin(df, attr_list):
+    nominal_attr = [attr for attr in attr_list
+                    if attr.level == dataConst.AttributeLevel.NOMINAL
+                        and attr not in dataConst.ID_FIELDS]
+    scale_attr = [attr for attr in attr_list
+                    if attr.level == dataConst.AttributeLevel.SCALE
+                        and attr not in dataConst.ID_FIELDS]
+    nom_colname = [attr.variable for attr in nominal_attr
+                    if attr.variable in df.columns]
+    scale_colname = [attr.variable for attr in scale_attr
+                    if attr.variable in df.columns]
+
+    corr_matrix = []
+    for i in nom_colname:
+        result = []
+        for j in scale_colname:
+            tmp_df = df[[i, j]]
+            category_group_list = tmp_df.groupby(i)[j].apply(list)
+            stat, p = f_oneway(*category_group_list)
+            if p <= 0.05:
+                result.append(1)
+            else:
+                result.append(0)
+        corr_matrix.append(result)
+
+    fig = plt.figure(figsize=(10.41, 7.29))
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(corr_matrix, vmin=-1, vmax=1)
+    fig.colorbar(cax)
+    plt.savefig("nominal_scale_correlation.png")
+
+
 def filter_correlated_attributes(df, attr_list, level, preprocessor):
     level_attrs = [attr for attr in attr_list 
         if (attr.level == level
             and attr.variable not in dataConst.ID_FIELDS)]
 
     return preprocessor.filter_correlated_columns(df, level_attrs)
+
+
+def filter_correlated_nominal_scale_attributes(df, attr_list):
+    nominal_attr = [attr for attr in attr_list
+                    if attr.level == dataConst.AttributeLevel.NOMINAL
+                        and attr not in dataConst.ID_FIELDS]
+    scale_attr = [attr for attr in attr_list
+                    if attr.level == dataConst.AttributeLevel.SCALE
+                        and attr not in dataConst.ID_FIELDS]
+    nom_colname = [attr.variable for attr in nominal_attr
+                    if attr.variable in df.columns]
+    scale_colname = [attr.variable for attr in scale_attr
+                    if attr.variable in df.columns]
+
+    redundent_attrs = set()
+    for i in nom_colname:
+        for j in scale_colname:
+            tmp_df = df[[i, j]]
+            category_group_list = tmp_df.groupby(i)[j].apply(list)
+            stat, p = f_oneway(*category_group_list)
+            if p <= 0.05:
+                redundent_attrs.add(i)
+
+    return df.drop(labels=list(redundent_attrs), axis=1)
 
 
 def main():
@@ -62,18 +123,6 @@ def main():
         dataConst.AttributeLevel.NOMINAL,
         nominal_preprocess)
 
-
-    # visualize_correlation(
-    #     filtered_df, attr_list,
-    #     dataConst.AttributeLevel.SCALE,
-    #     scale_preprocess)
-
-    # visualize_correlation(
-    #     filtered_df,
-    #     attr_list,
-    #     dataConst.AttributeLevel.ORDINAL,
-    #     ordinal_preprocess)
-
     filtered_df = filter_correlated_attributes(
         filtered_df,
         attr_list,
@@ -85,6 +134,9 @@ def main():
         attr_list,
         dataConst.AttributeLevel.SCALE,
         scale_preprocess)
+
+    visualize_nominal_scale_correlatin(filtered_df, attr_list)
+    filtered_df = filter_correlated_nominal_scale_attributes(filtered_df, attr_list)
 
     validated_df = pd.concat([df[dataConst.ID_FIELDS], filtered_df], axis=1)
     validated_df.to_excel("valid.xlsx")
